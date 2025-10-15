71dba49e2a0c52ba551c65b066baf0c3e3ef7bab
Manash <shitmanash78@gmail.com>
2025-10-15 17:46:16 +0530

    server set up done

---
+++ client/.vite/deps/@radix-ui_react-slot.js
@@ -0,0 +1,15 @@
+import {
+  Slot,
+  Slottable,
+  createSlot,
+  createSlottable
+} from "./chunk-IIAO5NJP.js";
+import "./chunk-UNLUE3XC.js";
+import "./chunk-5WRI5ZAA.js";
+export {
+  Slot as Root,
+  Slot,
+  Slottable,
+  createSlot,
+  createSlottable
+};
